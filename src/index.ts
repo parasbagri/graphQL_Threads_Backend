e89e5d873816dae030d724b47d3bee0b533bf5bc
Paras Bagri <parasba85@gmail.com>
2024-12-03 14:15:29 +0530

    Setup PrismaClient, postgresDB,

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import {prismaClient} from './lib/DB'
 
 async function init(){
     const app = express();
@@ -14,14 +15,33 @@ const gqlServer = new ApolloServer({
     // schema definitions
     typeDefs: `
      type Query{
-     hello: String
-     say(name:String):String
+       hello: String
+       say(name:String):String
+     }
+     type Mutation {
+        createUser(firstName:String!, lastName:String!, email:String!, password:String!, profileImageUrl:String): Boolean
      }
     `, 
     resolvers: {
         Query:{
             hello: () => 'Hello, From GrapgQL world!',
             say: (_, {name }:{name:String}) => `Hello, ${name}! how are you!`
+        },
+        Mutation:{
+          createUser: async(_, {firstName, lastName, email, password, profileImageUrl}:{firstName:string, lastName:string, email:string, password:string , profileImageUrl:string}) => {
+            await prismaClient.user.create({
+              data: {
+                firstName,
+                lastName,
+                profileImageUrl,
+                email,
+                password,
+                solt: 'random_solt',
+              },
+            })
+             return true;
+          }
+
         }
     }
   });
